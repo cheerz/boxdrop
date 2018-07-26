9ef7d782bd7f393fb4333c13e585437c23306433
LeTchang <mvallet@student.42.fr>
2018-07-27 01:27:30 +0200

    add protocols

--- BoxDropFramework/BoxDropFramework/GameMVP/GameModel.swift
+++ BoxDropFramework/BoxDropFramework/GameMVP/GameModel.swift
@@ -1,4 +1,4 @@
-import Foundation
+import AVFoundation
 
 class GameModel {
 
@@ -7,4 +7,47 @@ class GameModel {
 // MARK: - Game.Model
 extension GameModel: Game.Model {
 
+    func setProgressListener(with progressListener: GameUploadingProgressListener) {
+        fatalError("\(#function) has not been implemented")
+    }
+
+    func updateProgress(id: String, progress: Float) {
+        fatalError("\(#function) has not been implemented")
+    }
+
+    func getProgress() -> Float {
+        fatalError("\(#function) has not been implemented")
+    }
+
+    func isUploadComplete() -> Bool {
+        fatalError("\(#function) has not been implemented")
+    }
+
+    func remainingPhotos() -> Int {
+        fatalError("\(#function) has not been implemented")
+    }
+
+    func getTotalProgress() -> Float {
+        fatalError("\(#function) has not been implemented")
+    }
+
+    func getVideoPlayer() -> AVPlayer? {
+        fatalError("\(#function) has not been implemented")
+    }
+
+    func isUploadedOrFailed() -> Bool {
+        fatalError("\(#function) has not been implemented")
+    }
+
+    func resetUploadIfNeeded() {
+        fatalError("\(#function) has not been implemented")
+    }
+
+    func getCurrentImage(handler: @escaping (UIImage?) -> ()) {
+        fatalError("\(#function) has not been implemented")
+    }
+
+    func newImageNeeded() -> Bool {
+        fatalError("\(#function) has not been implemented")
+    }
 }
--- BoxDropFramework/BoxDropFramework/GameMVP/GameViewControllerMVP.swift
+++ BoxDropFramework/BoxDropFramework/GameMVP/GameViewControllerMVP.swift
@@ -1,13 +1,40 @@
-import UIKit
+import AVFoundation
 
 enum Game {
-    typealias Model     = GameModelProtocol
-    typealias View      = GameViewProtocol
+    typealias Model = GameUploadingModelProtocol
+    typealias View = GameViewProtocol
     typealias Presenter = GamePresenterProtocol
 }
 
-protocol GameModelProtocol {
+public protocol GameUploadingProgressListener {
 
+    func onPhotoProgress(progress: Float, id: String)
+
+}
+
+public protocol GameUploadingModelProtocol {
+
+    func setProgressListener(with progressListener: GameUploadingProgressListener)
+
+    func updateProgress(id: String, progress: Float)
+
+    func getProgress() -> Float
+
+    func isUploadComplete() -> Bool
+
+    func remainingPhotos() -> Int
+
+    func getTotalProgress() -> Float
+
+    func getVideoPlayer() -> AVPlayer?
+
+    func isUploadedOrFailed() -> Bool
+
+    func resetUploadIfNeeded()
+
+    func getCurrentImage(handler: @escaping (UIImage?) -> ())
+
+    func newImageNeeded() -> Bool
 }
 
 protocol GameViewProtocol: class {
